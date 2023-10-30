234a3887cab5d87e40a3a2bf5bfd7d9d5f027ee6
baltitasnime <baltitasnime@gmail.com>
2023-10-30 21:22:29 +0100

    video preview

---
+++ WebFundamentals/Week 2/Day 1/Practice/VideoPreview/script.js
@@ -0,0 +1,8 @@
+console.log("page loaded...");
+
+function pause (cat) {
+    cat.pause()
+}
+function play (cat) {
+    cat.play()
+}
--- WebFundamentals/Week 2/Day 1/button clicker/script.js
+++
@@ -1,8 +0,0 @@
-function ninja(){
-  alert("ninja was liked")
-}
-
-function remove() {
-    var btn = document.getElementById('greenbtn');
-    btn.style.display = 'none';
-}
