010e2bf36aa71a7d8fd6c58c87c189b2dcc9faf4
baltitasnime <baltitasnime@gmail.com>
2023-10-31 21:03:35 +0100

    likes

---
+++ WebFundamentals/Week 2/Day 2/Core/likes/script.js
@@ -0,0 +1,24 @@
+var counter=3
+function increaselikes(){
+    var likes=document.querySelector("#cl")
+    counter++
+    likes.innerText=counter+"like(s)"
+
+
+}
+var counter1=12
+function increaselikes1(){
+    var likes=document.querySelector("#cll")
+    counter1++
+    likes.innerText=counter1+"like(s)"
+
+
+}
+var counter2=9
+function increaselikes2(){
+    var likes=document.querySelector("#yy")
+    counter2++
+    likes.innerText=counter2+"like(s)"
+
+
+}
