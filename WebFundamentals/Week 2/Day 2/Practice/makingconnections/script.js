a0c1e466f0ecaf502327e72d1165450ee141304c
baltitasnime <baltitasnime@gmail.com>
2023-11-01 11:57:31 +0100

    making connections

---
+++ WebFundamentals/Week 2/Day 2/Practice/makingconnections/script.js
@@ -0,0 +1,11 @@
+console.log("gggg");
+function changee() {
+    var name= document.querySelector(".name")
+    name.innerText="tasnime b"
+    
+}
+function remove() {
+    var request=document.querySelector(".friend")
+    request.remove()
+    
+}
