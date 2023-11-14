3325b50961b66651f8b1581dc26bf14ba88550bf
baltitasnime <baltitasnime@gmail.com>
2023-11-14 14:52:48 +0100

    loopbasic1

---
+++ Python/Week-1/Day-1/Core/loopbasic/loopbasic1.py
@@ -0,0 +1,40 @@
+# for i in range (151):
+#     print(i)
+
+
+# for i in range(5, 1001, 5):
+#     print(i)
+
+
+
+# for i in range(1, 101):
+#     if i % 10 == 0:
+#         print("Coding Dojo")
+#     elif i % 5 == 0:
+#         print("Coding")
+#     else:
+#         print(i)
+    
+
+# sum_of_odds =0
+# for i in range(1, 500001, 2):
+#     sum_of_odds += i
+# print(sum_of_odds)
+
+
+
+
+# for i in range(0,2018,4):
+#     print(i)
+
+
+
+lowNum= 3 
+highNum=11 
+mult=2
+for i in range(3,11):
+    if (i%2 == 0):
+     print(i)
+
+
+
---
+++ Python/more_opp.py
@@ -0,0 +1,26 @@
+class student:
+    def __init__(self, dict_data):
+        
+
+        self.name = dict_data['name']
+        self.age = dict_data ['age']
+        self.group = dict_data ['group']
+        self.fav_food =dict_data['fav_food']
+
+        def increase_age(self,years=1):
+            self.age+= years
+            print("happy birthday")
+            return self 
+        
+
+        def change_name(self , new_name):
+            self.name= new_name
+            return self
+
+
+john_data ={'name': "john",'age':21,group'A3'}
+
+
+
+
+        
