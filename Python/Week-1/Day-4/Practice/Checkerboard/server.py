b95bcd1614acdacd1b94656a973be054c74e40e1
baltitasnime <baltitasnime@gmail.com>
2023-11-17 22:49:02 +0100

    commit

--- Python/Week-1/Day-4/Practice/Checkerboard/server.py
+++ Python/Week-1/Day-4/Practice/Checkerboard/server.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,3 +9,31 @@ def index(x,y,color,color_1):
 
 if __name__=="__main__":
     app.run(debug=True)
+=======
+from flask import Flask , render_template
+
+app=Flask(__name__)
+
+@app.route ("/")
+def show():
+    return "hello world"
+
+
+@app.route("/show")
+def show_1():
+    return render_template("index.html",row=8,col=8)
+
+
+# @app.route("/color (int:number)")
+# def color():
+#    return render_template("index.html")
+
+
+
+
+if __name__ == "__main__":
+ app.run(debug=True)
+
+
+
+>>>>>>> d017090 (commit)
