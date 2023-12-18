8bbb1dd2b8abad699d3cc5dc98d4adf2fdd4f8f5
baltitasnime <baltitasnime@gmail.com>
2023-12-18 19:55:43 +0100

    commit

--- MERNStack/week_1/Day_1/Practice/hoist/hoist.js
+++ MERNStack/week_1/Day_1/Practice/hoist/hoist.js
@@ -6,15 +6,15 @@ var hello = 'world';
 var needle = 'haystack';
 test();
 function test(){
-    var needle = 'magnet';
-    console.log(needle);
+    var needle = 'magnet';
+    console.log(needle);
 }
 // magnet
 
 var brendan = 'super cool';
 function print(){
-       brendan = 'only okay';
-        console.log(brendan);
+       brendan = 'only okay';
+        console.log(brendan);
     }
 console.log(brendan);
 // super cool 
@@ -23,19 +23,19 @@ var food = 'chicken';
 console.log(food);
 eat();
 function eat(){
-    food = 'half-chicken';
-    console.log(food);
-    var food = 'gone';
+    food = 'half-chicken';
+    console.log(food);
+    var food = 'gone';
 }
 // chicken ; half-chicken
 
 mean();
 console.log(food);
 var mean = function() {
-    food = "chicken";
-    console.log(food);
-    var food = "fish";
-    console.log(food);
+    food = "chicken";
+    console.log(food);
+    var food = "fish";
+    console.log(food);
 }
 console.log(food);
 
@@ -46,10 +46,10 @@ console.log(genre);
 var genre = "disco";
 rewind();
 function rewind() {
-    genre = "rock";
-    console.log(genre);
-    var genre = "r&b";
-    console.log(genre);
+    genre = "rock";
+    console.log(genre);
+    var genre = "r&b";
+    console.log(genre);
 }
 console.log(genre);
 
@@ -63,10 +63,10 @@ dojo = "san jose";
 console.log(dojo);
 learn();
 function learn() {
-    dojo = "seattle";
-    console.log(dojo);
-    var dojo = "burbank";
-    console.log(dojo);
+    dojo = "seattle";
+    console.log(dojo);
+    var dojo = "burbank";
+    console.log(dojo);
 }
 console.log(dojo);
 
@@ -81,15 +81,15 @@ console.log(dojo);
 console.log(makeDojo("Chicago", 65));
 console.log(makeDojo("Berkeley", 0));
 function makeDojo(name, students){
-    const dojo = {};
-    dojo.name = name;
-    dojo.students = students;
-    if(dojo.students > 50){
-        dojo.hiring = true;
-    }
-    else if(dojo.students <= 0){
-        dojo = "closed for now";
-    }
-    return dojo;
+    const dojo = {};
+    dojo.name = name;
+    dojo.students = students;
+    if(dojo.students > 50){
+        dojo.hiring = true;
+    }
+    else if(dojo.students <= 0){
+        dojo = "closed for now";
+    }
+    return dojo;
 }
 //ERROR
